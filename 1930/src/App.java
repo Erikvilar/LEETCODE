cae1cd38ba3ee0911f8496825ed91507a86a336e
Erikvilar <erikvilaralves@gmail.com>
2025-01-08 23:16:00 -0300

    resolvendo com Math round

--- 1930/src/App.java
+++ 1930/src/App.java
@@ -3,14 +3,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Set;
 
-
 public class App {
+
     public static void main(String[] args) throws Exception {
 
-        System.out.println(countPalindromicSubsequence("adc"));
+        System.out.println(countPalindromicSubsequence("aabca"));
     }
 
     public static int countPalindromicSubsequence(String s) {
@@ -18,42 +17,42 @@ public class App {
         List<String> letter = Arrays.asList(s.split(""));
 
         List<String> result = new ArrayList<>();
-
-        for (int i = 0; i < 3; i++) {
-            String res = looping(letter);
-
-            result.add(res);
-        }
-
+        int count = letter.size();
         Set<String> norepeat = new LinkedHashSet<String>();
 
+        for (int i = 0; i < letter.size(); i++) {
+            String res = looping(letter, count);
+
+            if (!res.isEmpty()) {
+                result.add(res);
+            }
+            count--;
+        }
         for (String r : result) {
             norepeat.add(r);
         }
         System.out.println(norepeat);
-        return norepeat.size();
+
+        return result.size();
 
     }
 
-
-
-
-    private static String looping(List<String> s) {
-
+    private static String looping(List<String> s, int count) {
         StringBuilder t = new StringBuilder(3);
-        int count = s.size();
-        ListIterator l = s.listIterator();
         for (int i = 0; i < s.size(); i++) {
-            System.out.print(s.get(i) + "==  ");
-            if (s.get(i).equals(s.get(count - 1)) )  {
-                    t.append(s.get(i) + s.get(l.next() != s.get(i)? l.nextIndex() :i) + s.get(count - 1)+ "-");
-                    System.out.println("igualdade encontrada " + s.get(i) + "==" + s.get(count - 1));
-                    count--;
+
+            if (s.get(i).equals(s.get(count - 1)) && i != count - 1) {
+
+                t.append(s.get(i) + 
+                s.get((int) Math.round((Math.floor(count - 1 + i) / 2))+1) 
+                + s.get(count - 1));
+                System.out.println(Math.round((Math.floor(count - 1 + i) / 2)));
+                return t.toString();
+                
             }
+
         }
-            return t.toString();
-    }}
+        return t.toString();
+    }
 
-
-
-
+}
